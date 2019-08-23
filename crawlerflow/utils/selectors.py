2632c6cd84f4aedd9fe7892abae336b80e4fa710
Ravi Raja Merugu <rrmerugu@gmail.com>
2019-08-23 16:30:10 +0530

    transform fields path fixes

--- crawlerflow/utils/selectors.py
+++ crawlerflow/utils/selectors.py
@@ -21,7 +21,7 @@ class SelectorExtractor(object):
 
 
 def transform_data(data=None, data_type=None):
-    fields = importlib.import_module("crawlerflow.fields")
+    fields = importlib.import_module("crawlerflow.core.fields")
     Klass = getattr(fields, data_type)
     data = Klass(data=data).transform()
     return data
